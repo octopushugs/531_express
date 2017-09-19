7f00a2023f3a3f958875043970a17be3936fd2f8
Mike Foster <type.foster@gmail.com>
2017-09-19 13:59:49 -0700

    Implement formula for 1rm rounded to the nearest multpile of 5

--- routes/calculate.js
+++ routes/calculate.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-// Calculates data
-router.get('/', function(req, res, next) {
-  res.json({ requested_at: req.requestTime });
-});
+var _ = require('lodash');
+var object = require('lodash/fp/object');
 
+// calculates 1rm maxes
 router.post('/', function(req, res, next) {
-  console.log(req.body)
+  let maxes = { b: null, s: null, o: null, d: null };
+
+  // The body should only contain the values for the 4 main lifts, so we should
+  // be able to just iterate over the individual properties.
+  _.forOwn(req.body, function(value, key) {
+    if (value != null && value > 0) {
+      value = parseFloat(value);
+
+      // formula: 1rm = w(1 + r/30), assuming r > 1
+      maxes[key] = Math.round(value * (1 + (1.0/30.0)));
+
+      // then round to the nearest multiple of 5 for easy plate math
+      maxes[key] = Math.ceil(maxes[key]/5)*5;
+    }
+  });
+
   next();
 });
 
