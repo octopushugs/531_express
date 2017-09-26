8878bbb003085da98e74ba3418cd9fe78d281c6f
Mike Foster <type.foster@gmail.com>
2017-09-26 16:18:57 -0700

    Add math for calculating the 4-week routine

--- routes/calculate.js
+++ routes/calculate.js
@@ -8,6 +8,7 @@ const object = require('lodash/fp/object');
 // calculates 1rm maxes
 router.post('/', function(req, res, next) {
   let maxes = { b: null, s: null, o: null, d: null };
+  let program = { b: [], s: [], o: [], d: [] };
   let responseMarkup = "";
 
   // The body should only contain the values for the 4 main lifts, so we should
@@ -23,6 +24,24 @@ router.post('/', function(req, res, next) {
       maxes[key] = Math.ceil(maxes[key]/2.5)*2.5;
     }
   });
+
+  // The program will produce 4 weeks worth of sets/reps in a format that looks like this:
+  // { b: [max*0.65, max*0.75, max*0.85], [max*0.70, max*0.80, max*0.90], [max*0.75, max*0.85, max*0.95], [max*0.40, max*0.50, max*0.60] }
+  // where each array contains a weight as a percentage of calculated max
+
+  _.forOwn(maxes, function(value, key) {
+    value = parseFloat(value);
+
+    program[key] = [
+      [[5, 5, 5], [3, 3, 3], [5, 3, 1], [5, 5, 5]],
+      [value * 0.65, value * 0.75, value * 0.85],
+      [value * 0.70, value * 0.80, value * 0.90],
+      [value * 0.75, value * 0.85, value * 0.95],
+      [value * 0.40, value * 0.50, value * 0.60],
+    ];
+  });
+
+  console.log(program);
 
   responseMarkup = pug.renderFile('./views/max_results.pug', maxes);
   res.send(responseMarkup);
